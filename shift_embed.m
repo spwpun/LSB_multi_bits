2a0b6bfd1f3d17f40430b54729a30af5f4e96d5e
spwpun <spwpun@gmail.com>
2019-03-27 15:51:45 +0800

    Improved by using function randperm()

--- shift_embed.m
+++ shift_embed.m
@@ -1,7 +1,8 @@
-function [] = shift_embed(im_name, sec_name)
+function [] = shift_embed(im_name, sec_name, key)
 %该函数把一张秘密图片进行灰度化之后嵌入到另一张彩色图片中，并通过自定义的函数计算嵌入前后图片的MSE
 %im_name：该参数为载体图片的文件名
 %sec_name：该参数为秘密文件的文件名
+%key:该参数为了确定安全性，用来生成随机矩阵
 im = imread(im_name);%得到载体图片的rgb模式图
 subplot(1,2,1);imshow(im);title('Origin');
 sec = imread(sec_name);
@@ -25,32 +26,36 @@ shiftsecG = bitand(shiftsecG,7);
 B_ = bitand(B,252);
 shiftsecB = bitand(sec_gray,3);
 
+%以特定的key生成特定的随机矩阵来存放秘密图片的信息
+npoints = row1*col1;
+rand('state', key);
+position = randperm(npoints);
+
 if(row2*col2>(row1*col1-20))  %R层的前20LSB位存储秘密信息的宽和高信息
     fprintf('You should choose another more smaller image!\n');
     warning('Too large,the information maybe part disappear!\n');
 else
-    %R层的前20个LSB位存储秘密信息的宽和高信息，只有R层存储信息，后面G层和B层就可以直接放了
+    %R层的随机矩阵前20个LSB位存储秘密信息的宽和高信息，只有R层存储信息，后面G层和B层就可以直接放了
     for i = 1:20
-        R_(i) = bitset(R_(i),1,size_info(i));
+        R_(position(i)) = bitset(R_(position(i)),1,size_info(i));
     end
     %store the image info in the rest of cover;
     for j=1:row2*col2
-        index = j+20;
-        R_(index) = bitor(R_(index),shiftsecR(j));%将秘密图片的高三位放到载体图片R层的低3位
+        R_(position(j+20)) = bitor(R_(position(j+20)),shiftsecR(j));%将秘密图片的高三位放到载体图片R层的低3位
     end
     %将修改后的R层的数据存入原图片，并计算MSE
     im(:,:,1) = R_;
     MSE_R = CalcuMSE(R,R_);
     %将秘密图片的中三位放到载体图片G层的低3位
     for k=1:row2*col2
-        G_(k+20) = bitor(G_(k+20),shiftsecG(k));%将秘密图片的高三位放到载体图片的低3位
+        G_(position(k)) = bitor(G_(position(k)),shiftsecG(k));%将秘密图片的高三位放到载体图片的低3位
     end
     %将修改后的G层的数据存入原图片，并计算MSE
     im(:,:,2) = G_;
     MSE_G = CalcuMSE(G,G_);
     %将秘密图片的低2位放到载体图片B层的低2位
     for l=1:row2*col2
-        B_(l+20) = bitor(B_(l+20),shiftsecB(l));%将秘密图片的高三位放到载体图片的低3位
+        B_(position(l)) = bitor(B_(position(l)),shiftsecB(l));%将秘密图片的高三位放到载体图片的低3位
     end
     %将修改后的B层的数据存入原图片，并计算MSE
     im(:,:,3) = B_;
--- shift_extract.m
+++ shift_extract.m
@@ -1,40 +1,46 @@
-function [] = shift_extract(steg_im)
+function [] = shift_extract(steg_im, key)
 %该函数是从一张嵌入了秘密图片的彩色图片中提取出秘密图片的过程
 %steg_im:该参数为彩色图片放入文件名或者路径.
+%key:该参数应该与嵌入端的key相同，这样才可以生成相同的随机矩阵
 steg = imread(steg_im);
 R = steg(:,:,1);
 G = steg(:,:,2);
 B = steg(:,:,3);
+%生成同嵌入端一致的随机矩阵
+[row, col] = size(rgb2gray(steg));
+rand('state', key);
+pos = randperm(row*col);
+
 %预设置秘密图片的高与宽，并从R层提取出图片的高和宽
 height = zeros(1,10);
 width = zeros(1,10);
 %提取秘密图片的高
 for h = 1:10
-    height(h) = bitget(R(h),1);
+    height(h) = bitget(R(pos(h)),1);
 end
 %提取出秘密图片的宽
 for w = 11:20
-    width(w-10) = bitget(R(w),1);
+    width(w-10) = bitget(R(pos(w)),1);
 end
 %转成十进制
 height = bi2de(height);
 width = bi2de(width);
-fprintf('%d*%d\n',height,width);
+fprintf('The secret image size is %d*%d\n',height,width);
 %设置秘密图片的高和宽
 R_ = zeros(height,width);
 G_ = zeros(height,width);
 B_ = zeros(height,width);
 %提取R层的秘密信息
 for i = 1:height*width
-    R_(i) = bitand(R(i+20),7);%得到R层的低三位
+    R_(i) = bitand(R(pos(i+20)),7);%得到R层的低三位
 end
 %提取G层的秘密信息
 for j = 1:height*width
-    G_(j) = bitand(G(j),7);%得到G层的低三位
+    G_(j) = bitand(G(pos(j)),7);%得到G层的低三位
 end
 %提取R层的秘密信息
 for k = 1:height*width
-    B_(k) = bitand(B(k),3);%得到B层的低二位
+    B_(k) = bitand(B(pos(k)),3);%得到B层的低二位
 end
 R_ = bitshift(R_,5);% 将R层的3位数据移动到高3位
 G_ = bitshift(G_,2);% 将G层的数据左移两位移动到中间3位
